7920ca2078112e9a2df03f727928d70f3ebf8c01
daveman-creator <djustin43@gmail.com>
2023-04-15 11:38:45 +0200

    Modify the PWA settings

--- app/_document.js
+++ app/_document.js
@@ -1,21 +1,21 @@
-import Document, { Head, Html, Main, NextScript } from 'next/document';
+// import Document, { Head, Html, Main, NextScript } from 'next/document';
 
-// 41.1K (gzipped: 12.4K) // Path: app\_document.js
+// // 41.1K (gzipped: 12.4K) // Path: app\_document.js
 
-export default class MyDocument extends Document {
-  render() {
-    return (
-      <Html>
-        <Head>
-          <link rel="manifest" href="/manifest.json" />
-          <link rel="apple-touch-icon" href="/icon.png" />
-          <meta name="theme-color" content="#317EFB" />
-          <body>
-            <Main />
-            <NextScript />
-          </body>
-        </Head>
-      </Html>
-    );
-  }
-}
+// export default class MyDocument extends Document {
+//   render() {
+//     return (
+//       <Html>
+//         <Head>
+//           <link rel="manifest" href="/manifest.json" />
+//           <link rel="apple-touch-icon" href="/icon.png" />
+//           <meta name="theme-color" content="#317EFB" />
+//           <body>
+//             <Main />
+//             <NextScript />
+//           </body>
+//         </Head>
+//       </Html>
+//     );
+//   }
+// }
